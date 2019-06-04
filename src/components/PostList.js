d3aca3e30991ce8f81f449c965089025d74d0423
rumeetsingh <33159612+rumeetsingh@users.noreply.github.com>
2019-06-04 11:57:24 +0530

    Reducers addded

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -7,14 +7,33 @@ class PostList extends React.Component {
 
     componentDidMount() {
         this.props.fetchPosts()
-    }
+    };
+
+    renderList = () => {
+        return this.props.posts.map(post => {
+            return (
+                <div className="card mt-3 mb-3" key={post.id}>
+                    <div className="card-body">
+                        <h5 className="card-title">{post.title}</h5>
+                        <p className="card-text">{post.body}</p>
+                    </div>
+                    <div className="card-footer"><small>{post.userId}</small></div>
+                </div>
+            );
+        });
+    };
 
     render() {
         return (
-            <div>PostList</div>
+            <div>
+                {this.renderList()}
+            </div>
         );
     };
 }
 
+const mapStateToProps = (state) => {
+    return { posts : state.posts };
+}
 
-export default connect(null,{ fetchPosts : fetchPosts })(PostList);
+export default connect(mapStateToProps,{ fetchPosts : fetchPosts })(PostList);
